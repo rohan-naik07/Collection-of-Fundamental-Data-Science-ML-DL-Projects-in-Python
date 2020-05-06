4507ba1f8ae262913db77a8f3e57e15323a177f4
rohanrohan2012 <61932540+rohanrohan2012@users.noreply.github.com>
2020-05-06 13:43:42 +0530

    Delete app2.py

--- catsdogs/flask-api/app2.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.datasets import fashion_mnist
-from scipy.misc import imsave
-
-(X_train,Y_train),(X_test,Y_test)=fashion_mnist.load_data()
-
-for i in range(5):
-    imsave(name="uploads/{}.png".format(i),arr=X_test[i])
